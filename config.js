f61828d05ecd8da62f8b60825f58ab0a0428f1cd
MularFF <ogmular@gmail.com>
2025-01-09 06:20:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "2347013349642,234xxxxx";
-
+global.react_status = process.env.AUTO_REACT_STATUS || "true";
 global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://x-bot-session-generator.onrender.com";
 
